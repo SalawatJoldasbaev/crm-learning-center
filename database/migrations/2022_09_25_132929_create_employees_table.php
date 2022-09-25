f835cb15669a37973927eb8405e5b41c498d7d41
SalawatJoldasbaev <jsalawatdeveloper@gmail.com>
2022-09-25 23:20:22 +0500

    first commit

---
+++ database/migrations/2022_09_25_132929_create_employees_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\File;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(File::class);
+            $table->string('name');
+            $table->string('phone', 13)->unique();
+            $table->string('password');
+            $table->enum('role', ['ceo', 'teacher']);
+            $table->double('salary_percentage')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+};
